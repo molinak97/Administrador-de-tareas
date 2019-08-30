47571a927e23b23607862796a7dc088fcc4603c0
Kevin Molina <molinavillanuevak@gmail.com>
2019-08-29 22:04:13 -0500

    Se crean los 3 hilos y se comienzan las validaciones

--- AdministradorTareas/Form1.Designer.cs
+++ AdministradorTareas/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonWait = new System.Windows.Forms.Button();
+            this.buttonResume = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -69,11 +73,55 @@
             this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.columnHeader2.Width = 117;
             // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(263, 69);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "Iniciar";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(263, 98);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonWait
+            // 
+            this.buttonWait.Location = new System.Drawing.Point(263, 127);
+            this.buttonWait.Name = "buttonWait";
+            this.buttonWait.Size = new System.Drawing.Size(75, 23);
+            this.buttonWait.TabIndex = 5;
+            this.buttonWait.Text = "Wait";
+            this.buttonWait.UseVisualStyleBackColor = true;
+            this.buttonWait.Click += new System.EventHandler(this.buttonWait_Click);
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Location = new System.Drawing.Point(263, 157);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(75, 23);
+            this.buttonResume.TabIndex = 6;
+            this.buttonResume.Text = "Resume";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            this.buttonResume.Click += new System.EventHandler(this.buttonResume_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 323);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.buttonWait);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonRun);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -89,6 +137,10 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonWait;
+        private System.Windows.Forms.Button buttonResume;
     }
 }
 
--- AdministradorTareas/Form1.cs
+++ AdministradorTareas/Form1.cs
@@ -13,39 +13,143 @@ namespace AdministradorTareas
 {
     public partial class Form1 : Form
     {
-        Thread hilo;
         delegate void delegado(int valor);
+        Thread Tarea1, Tarea2, Tarea3;
+
 
         public Form1()
         {
             InitializeComponent();
-
-
-
-            hilo = new Thread(new ThreadStart(Proceso1));
-            hilo.Start();
-            hilo = new Thread(new ThreadStart(Proceso2));
-            hilo.Start();
-            hilo = new Thread(new ThreadStart(Proceso3));
-            hilo.Start();
-            hilo = new Thread(new ThreadStart(Proceso4));
-            hilo.Start();
+            Tarea1 = new Thread(new ThreadStart(Proceso1));
+            Tarea2 = new Thread(new ThreadStart(Proceso2));
+            Tarea3 = new Thread(new ThreadStart(Proceso3));
+            for (int x = 1; x <= 3; x++)
+            {
+                ListViewItem item = new ListViewItem("Proceso " + x);
+                item.SubItems.Add(Tarea1.ThreadState.ToString());
+                listView1.Items.Add(item);
+            }
+            listView1.Items[0].Selected = true;
+            listView1.Select();
+            Run();
         }
         private void Proceso1()
         {
+            for(int i=0; i<=10000; i++)
+            {
 
+            }
         }
         private void Proceso2()
         {
+            for (int i = 0; i <= 10000; i++)
+            {
 
+            }
         }
         private void Proceso3()
         {
+            for (int i = 0; i <= 10000; i++)
+            {
 
+            }
         }
-        private void Proceso4()
+
+        private void buttonRun_Click(object sender, EventArgs e)
         {
-
+            Validar();
+            switch (listView1.FocusedItem.Index)
+            {
+                case 0:
+                    Tarea1.Start();
+                    listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+                    break;
+                case 1:
+                    Tarea2.Start();
+                    listView1.Items[1].SubItems[1].Text = Tarea2.ThreadState.ToString();
+                    break;
+                case 2:
+                    Tarea3.Start();
+                    listView1.Items[2].SubItems[1].Text = Tarea3.ThreadState.ToString();
+                    break;
+                default:
+                    break;
+            }
         }
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            Validar();
+            switch (listView1.FocusedItem.Index)
+            {
+                case 0:
+                    Tarea1.Abort();
+                    listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+                    break;
+                case 1:
+                    Tarea2.Abort();
+                    listView1.Items[1].SubItems[1].Text = Tarea2.ThreadState.ToString();
+                    break;
+                case 2:
+                    Tarea2.Start();
+                    listView1.Items[2].SubItems[1].Text = Tarea3.ThreadState.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void buttonWait_Click(object sender, EventArgs e)
+        {
+            Validar();
+            switch (listView1.FocusedItem.Index)
+            {
+                case 0:
+                    Tarea1.Suspend();
+                    listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+                    break;
+                case 1:
+                    Tarea2.Suspend();
+                    listView1.Items[1].SubItems[1].Text = Tarea2.ThreadState.ToString();
+                    break;
+                case 2:
+                    Tarea3.Suspend();
+                    listView1.Items[2].SubItems[1].Text = Tarea3.ThreadState.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            Validar();
+            switch (listView1.FocusedItem.Index)
+            {
+                case 0:
+                    Tarea1.Resume();
+                    listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+                    break;
+                case 1:
+                    Tarea2.Resume();
+                    listView1.Items[1].SubItems[1].Text = Tarea2.ThreadState.ToString();
+                    break;
+                case 2:
+                    Tarea3.Resume();
+                    listView1.Items[2].SubItems[1].Text = Tarea3.ThreadState.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void Run()
+        {
+            Tarea1.Start();
+            listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+        }
+        private void Validar()
+        {
+            listView1.Items[0].SubItems[1].Text = Tarea1.ThreadState.ToString();
+            listView1.Items[1].SubItems[1].Text = Tarea1.ThreadState.ToString();
+            listView1.Items[2].SubItems[1].Text = Tarea1.ThreadState.ToString();
+        }
+
     }
 }
