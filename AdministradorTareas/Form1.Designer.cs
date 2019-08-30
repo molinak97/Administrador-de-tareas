e0f9908fecf8b9d670b2f905e6a7c161ae764d53
Kevin Molina <molinavillanuevak@gmail.com>
2019-08-29 20:39:07 -0500

    Se Comienza a manejar con hilos

--- AdministradorTareas/Form1.Designer.cs
+++ AdministradorTareas/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Administrador de Procesos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(39, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 207);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TAREA";
+            this.columnHeader1.Width = 66;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ESTADO";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 117;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(554, 323);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Administrador de procesos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- AdministradorTareas/Form1.cs
+++ AdministradorTareas/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace AdministradorTareas
 {
     public partial class Form1 : Form
     {
+        Thread hilo;
+        delegate void delegado(int valor);
+
         public Form1()
         {
             InitializeComponent();
+
+
+
+            hilo = new Thread(new ThreadStart(Proceso1));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Proceso2));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Proceso3));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Proceso4));
+            hilo.Start();
+        }
+        private void Proceso1()
+        {
+
+        }
+        private void Proceso2()
+        {
+
+        }
+        private void Proceso3()
+        {
+
+        }
+        private void Proceso4()
+        {
+
         }
     }
 }
